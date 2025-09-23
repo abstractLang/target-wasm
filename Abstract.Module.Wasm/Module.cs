868a1390f177c059983bda6a416a5513aa3e0f8e
Camila <94462076+lumi2021@users.noreply.github.com>
2025-09-23 15:48:59 -0300

    module bruh

---
+++ Abstract.Module.Wasm/Module.cs
@@ -0,0 +1,34 @@
+﻿using Abstract.Module.Core;
+using Abstract.Module.Core.Configuration;
+using Abstract.Realizer.Core.Configuration.LangOutput;
+using BetaIsa =  Abstract.Realizer.Core.Configuration.LangOutput.BetaExtendableInstructionSet;
+
+namespace Abstract.Module.Wasm;
+
+public class Module : IModule
+{
+    public ModuleConfiguration Config { get; } = new()
+    {
+        Name = "Webassembly",
+        Description = "Provides targets for webassembly bytecode",
+        
+        Author = "lumi2021",
+        Version = "1.0.0",
+        
+        Targets = [
+            new ModuleLanguageTargetConfiguration
+            {
+                TargetName = "Wasm",
+                TargetDescription = "Compiles to webassembly",
+                TargetIdentifier = "wasm",
+                
+                LanguageOutput = new BetaOutputConfiguration() {
+                    EnabledOpcodes = BetaIsa.None
+                        | BetaIsa.Dup
+                        | BetaIsa.Swap,
+                    SizedOperations = false,
+                }
+            }
+        ]
+    };
+}
